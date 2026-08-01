d6f28b138cb162278f46cdc9be7f259c2c5b96aa
Bavly Medhat <tv2tx7gnq4@privaterelay.appleid.com>
2026-08-01 06:07:37 +0300

    Add files via upload

---
+++ api-zod/dist/index.d.ts
@@ -0,0 +1,3 @@
+export * from "./generated/api";
+export type { ContactInput, Contact, CampaignInput, Campaign, CampaignDetail, CampaignStats, ExportData, ExportRow, ContactUpdate, SendResult, HealthStatus, } from "./generated/types";
+//# sourceMappingURL=index.d.ts.map
---
+++ api-zod/src/index.ts
@@ -0,0 +1,14 @@
+export * from "./generated/api";
+export type {
+  ContactInput,
+  Contact,
+  CampaignInput,
+  Campaign,
+  CampaignDetail,
+  CampaignStats,
+  ExportData,
+  ExportRow,
+  ContactUpdate,
+  SendResult,
+  HealthStatus,
+} from "./generated/types";
